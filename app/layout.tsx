8fa97488850872525f44ecc8259c82138362d91d
Inioluwa Sogelola <inioluwwa4.is@gmail.com>
2025-06-25 14:33:38 +0100

    fix: remove unnecessary dark class from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className="dark text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
+            className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
         >
             <head>
                 <link rel="preconnect" href="https://rsms.me/" />
